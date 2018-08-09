fbdf518021d71aab2ccca8aa1037775b0074d139
Mantas <mntsss@gmail.com>
2018-08-09 17:55:49 +0300

    Added Items component

---
+++ resources/assets/js/components/Items.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <div class="card">
+      <div class="card-header">
+
+      </div>
+      <div class="card-body">
+        <div class="row remove-side-margin">
+
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    data(){
+      return {
+        
+      }
+    }
+  }
+</script>
--- resources/assets/js/components/Main.vue
+++ resources/assets/js/components/Main.vue
@@ -16,11 +16,8 @@
           </div>
         </div>
         <div class="row item-name-field remove-all-margin">
-            <div class="col-9 item-text remove-all-padding">
+            <div class="col-12 item-text remove-all-padding">
               {{group.ItemGroupName}}
-            </div>
-            <div class="col-3 text-right remove-all-padding">
-              <a class="item-status"><span class="fas fa-angle-double-down text-warning"></span></a>
             </div>
         </div>
     </div>
